5996804b995a3e3cb1f6a70f7adb4d10d1cda27d
Trung Nguyen <trungntdev@gmail.com>
2022-10-13 16:55:06 +0700

    init project

--- NeighborlyAPI/getAdvertisements/__init__.py
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -1,14 +1,16 @@
+import os
 import azure.functions as func
 import pymongo
 import json
 from bson.json_util import dumps
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+        url = os.environ["MyDbConnection"]   # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['db-uda-2']
         collection = database['advertisements']
 
 
--- NeighborlyAPI/getPosts/__init__.py
+++ NeighborlyAPI/getPosts/__init__.py
@@ -3,16 +3,16 @@ import azure.functions as func
 import pymongo
 import json
 from bson.json_util import dumps
-
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python getPosts trigger function processed a request.')
 
     try:
-        url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+        url = os.environ["MyDbConnection"]   # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['db-uda-2']
         collection = database['posts']
 
         result = collection.find({})
